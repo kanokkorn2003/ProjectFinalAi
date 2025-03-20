a839a1b1575036d5e26c28d91149eaeb7b27afe3
Kanokmanee64577 <Kanokmanee.j@ku.th>
2025-03-21 03:07:19 +0700

    ข้อความ commit

--- src/aboutus/aboutus.js
+++ src/aboutus/aboutus.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './aboutus.css'; // ใช้สำหรับการจัดแต่งสไตล์ของหน้า
 import aboutusImage from './aboutus.png';
-import endImage from './end.png';
 import ourvitionImage from './ourvition.png';
+
 
 const aboutus = () => {
   return (
@@ -32,15 +32,6 @@ const aboutus = () => {
         />
       </section>
     
-     
- {/* Image Section */}
- <section className="about-image">
-        <img
-          src={endImage} // เปลี่ยนเป็นลิงก์ภาพที่ต้องการ
-          alt="สุขภาพจิต"
-          className="about-image-img"
-        />
-      </section>
      
 
 
